9751a6a39b5c5f378a7521c651eb2b8bfe9121fd
Mariane Vilarim <vilarim.mariane@gmail.com>
2022-07-19 18:21:54 -0300

    introdução da tranding

---
+++ Projeto 3/View/TrandingTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TrandingTableViewCell.swift
+//  Projeto 3
+//
+//  Created by Mariane Vilarim on 19/07/22.
+//
+
+import UIKit
+
+class TrandingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    //func setup (title: String, imagem: UIImage, year: String){
+        
+    //}
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
